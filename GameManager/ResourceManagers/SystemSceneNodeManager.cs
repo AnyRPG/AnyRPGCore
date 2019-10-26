b0a8ea7a3e8d12f353242e4aa2dc249a85655a3c
Michael Day <github@mday.ca>
2019-10-26 01:43:17 -0700

    Add null scene node check to SystemSceneNodeManager

--- GameManager/ResourceManagers/SystemSceneNodeManager.cs
+++ GameManager/ResourceManagers/SystemSceneNodeManager.cs
@@ -1,70 +1,73 @@
 using AnyRPG;
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace AnyRPG {
 
-/// <summary>
-/// allow us to query scriptable objects for equivalence by storing a template ID on all instantiated objects
-/// </summary>
-public class SystemSceneNodeManager : SystemResourceManager {
+    /// <summary>
+    /// allow us to query scriptable objects for equivalence by storing a template ID on all instantiated objects
+    /// </summary>
+    public class SystemSceneNodeManager : SystemResourceManager {
 
-    #region Singleton
-    private static SystemSceneNodeManager instance;
+        #region Singleton
+        private static SystemSceneNodeManager instance;
 
-    public static SystemSceneNodeManager MyInstance {
-        get {
-            if (instance == null) {
-                instance = FindObjectOfType<SystemSceneNodeManager>();
-            }
+        public static SystemSceneNodeManager MyInstance {
+            get {
+                if (instance == null) {
+                    instance = FindObjectOfType<SystemSceneNodeManager>();
+                }
 
-            return instance;
-        }
-    }
-    #endregion
-
-    const string resourceClassName = "SceneNode";
-
-    protected override void Awake() {
-        //Debug.Log(this.GetType().Name + ".Awake()");
-        base.Awake();
-    }
-
-    public override void LoadResourceList() {
-        //Debug.Log(this.GetType().Name + ".LoadResourceList()");
-        rawResourceList = Resources.LoadAll<SceneNode>(resourceClassName);
-        base.LoadResourceList();
-    }
-
-    public SceneNode GetResource(string resourceName) {
-        //Debug.Log(this.GetType().Name + ".GetResource(" + resourceName + ")");
-        if (!RequestIsEmpty(resourceName)) {
-            string keyName = prepareStringForMatch(resourceName);
-            if (resourceList.ContainsKey(keyName)) {
-                return (resourceList[keyName] as SceneNode);
+                return instance;
             }
         }
-        return null;
-    }
+        #endregion
 
+        const string resourceClassName = "SceneNode";
 
-    public List<SceneNode> GetResourceList() {
-        List<SceneNode> returnList = new List<SceneNode>();
-
-        foreach (UnityEngine.Object listItem in resourceList.Values) {
-            returnList.Add(listItem as SceneNode);
+        protected override void Awake() {
+            //Debug.Log(this.GetType().Name + ".Awake()");
+            base.Awake();
         }
-        return returnList;
-    }
 
-    public void LoadSceneNode(SceneNodeSaveData sceneNodeSaveData) {
-        //Debug.Log("QuestLog.LoadQuest(" + questSaveData.MyName + ")");
+        public override void LoadResourceList() {
+            //Debug.Log(this.GetType().Name + ".LoadResourceList()");
+            rawResourceList = Resources.LoadAll<SceneNode>(resourceClassName);
+            base.LoadResourceList();
+        }
 
-        SceneNode sceneNode = GetResource(sceneNodeSaveData.MyName);
-        sceneNode.MyCutsceneViewed = sceneNodeSaveData.isCutSceneViewed;
+        public SceneNode GetResource(string resourceName) {
+            //Debug.Log(this.GetType().Name + ".GetResource(" + resourceName + ")");
+            if (!RequestIsEmpty(resourceName)) {
+                string keyName = prepareStringForMatch(resourceName);
+                if (resourceList.ContainsKey(keyName)) {
+                    return (resourceList[keyName] as SceneNode);
+                }
+            }
+            return null;
+        }
+
+
+        public List<SceneNode> GetResourceList() {
+            List<SceneNode> returnList = new List<SceneNode>();
+
+            foreach (UnityEngine.Object listItem in resourceList.Values) {
+                returnList.Add(listItem as SceneNode);
+            }
+            return returnList;
+        }
+
+        public void LoadSceneNode(SceneNodeSaveData sceneNodeSaveData) {
+            //Debug.Log("QuestLog.LoadQuest(" + questSaveData.MyName + ")");
+
+            SceneNode sceneNode = GetResource(sceneNodeSaveData.MyName);
+            if (sceneNode == null) {
+                Debug.LogError("SystemSceneNodeManager.LoadSceneNode(): Scene " + sceneNodeSaveData.MyName + " could not be found.");
+            }
+            sceneNode.MyCutsceneViewed = sceneNodeSaveData.isCutSceneViewed;
+        }
     }
-}
 
 }
