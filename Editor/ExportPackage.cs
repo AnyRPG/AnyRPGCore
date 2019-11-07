a399dc5ef67b4445998dd8f1dda7fb8d6cb665c0
Michael Day <github@mday.ca>
2019-11-07 01:01:49 -0800

    Add Full export Editor script to allow export of layers and build settings

---
+++ Editor/ExportPackage.cs
@@ -0,0 +1,11 @@
+﻿// ExportPackage.cs
+using UnityEngine;
+using UnityEditor;
+
+public class ExportPackage {
+    [MenuItem("AnyRPG/Export/FullExport")]
+    static void export() {
+        AssetDatabase.ExportPackage(AssetDatabase.GetAllAssetPaths(), PlayerSettings.productName + ".unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies | ExportPackageOptions.IncludeLibraryAssets);
+    }
+}
+
