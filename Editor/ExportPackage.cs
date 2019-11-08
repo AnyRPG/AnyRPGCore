28e94ec6f8cbc1c37bd3703feedca90112abe055
Michael Day <github@mday.ca>
2019-11-07 19:52:01 -0800

    Updated ExportPackage script to use Asset Store recommendation Tools Menu as top level menu

--- Editor/ExportPackage.cs
+++ Editor/ExportPackage.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEditor;
 
 public class ExportPackage {
-    [MenuItem("AnyRPG/Export/FullExport")]
+    [MenuItem("Tools/AnyRPG/Export/FullExport")]
     static void export() {
         AssetDatabase.ExportPackage(AssetDatabase.GetAllAssetPaths(), PlayerSettings.productName + ".unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies | ExportPackageOptions.IncludeLibraryAssets);
     }
