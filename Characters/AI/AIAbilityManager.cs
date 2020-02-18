fabd4ac0c181f72e6bc61c542dbc53b2305d691b
Michael Day <github@mday.ca>
2020-02-17 22:22:34 -0800

    AI now auto-targets ground spells at target foot location

--- Characters/AI/AIAbilityManager.cs
+++ Characters/AI/AIAbilityManager.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 
 namespace AnyRPG {
     public class AIAbilityManager : CharacterAbilityManager {
+        protected override void BeginAbilityCommon(IAbility ability, GameObject target) {
 
+            base.BeginAbilityCommon(ability, target);
+            if (currentCastAbility != null && currentCastAbility.MyRequiresGroundTarget == true) {
+                Vector3 groundTarget = Vector3.zero;
+                if (baseCharacter != null && baseCharacter.MyCharacterController != null && baseCharacter.MyCharacterController.MyTarget != null) {
+                    groundTarget = baseCharacter.MyCharacterController.MyTarget.transform.position;
+                }
+                SetGroundTarget(groundTarget);
+            }
+        }
     }
 
 }
