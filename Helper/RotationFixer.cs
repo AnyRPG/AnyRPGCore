0ea7a0ad70655f1201c7adbc0c31ac078ebbff05
Michael Day <github@mday.ca>
2020-03-22 23:15:02 -0700

    add rotation fixer for pushable objects

---
+++ Helper/RotationFixer.cs
@@ -0,0 +1,19 @@
+﻿using AnyRPG;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script helps rotation constraints work properly by resetting rotation inertia settings, which can cause rotation constraints to not work properly
+
+namespace AnyRPG {
+    public class RotationFixer : MonoBehaviour {
+
+        void Start() {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.centerOfMass = Vector3.zero;
+            rb.inertiaTensorRotation = Quaternion.identity;
+        }
+
+    }
+
+}
