f55e2435fb7335126a4dd239f15b34751e527b57
Michael Day <github@mday.ca>
2020-12-13 11:59:19 -0800

    crafting nodes will use the ability as part of their prerequisites

--- Interactables/CraftingNodeComponent.cs
+++ Interactables/CraftingNodeComponent.cs
@@ -15,6 +15,43 @@ namespace AnyRPG {
         public CraftingNodeComponent(Interactable interactable, CraftingNodeProps interactableOptionProps) : base(interactable, interactableOptionProps) {
         }
 
+        public override void CreateEventSubscriptions() {
+            //Debug.Log("GatheringNode.CreateEventSubscriptions()");
+            if (eventSubscriptionsInitialized) {
+                return;
+            }
+            base.CreateEventSubscriptions();
+
+            // because the skill is a special type of prerequisite, we need to be notified when it changes
+            if (SystemEventManager.MyInstance == null) {
+                Debug.LogError("SystemEventManager Not Found.  Is the GameManager prefab in the scene?");
+                return;
+            }
+            SystemEventManager.MyInstance.OnAbilityListChanged += HandleAbilityListChange;
+        }
+
+        public override void CleanupEventSubscriptions() {
+            //Debug.Log("GatheringNode.CleanupEventSubscriptions()");
+            if (!eventSubscriptionsInitialized) {
+                return;
+            }
+            base.CleanupEventSubscriptions();
+
+            if (SystemEventManager.MyInstance != null) {
+                SystemEventManager.MyInstance.OnAbilityListChanged -= HandleAbilityListChange;
+            }
+        }
+
+        public void HandleAbilityListChange(BaseAbility baseAbility) {
+            //Debug.Log(gameObject.name + ".GatheringNode.HandleAbilityListChange(" + baseAbility.DisplayName + ")");
+            HandlePrerequisiteUpdates();
+        }
+
+        public override int GetCurrentOptionCount() {
+            //Debug.Log(gameObject.name + ".GatheringNode.GetCurrentOptionCount()");
+            return ((PlayerManager.MyInstance.MyCharacter.CharacterAbilityManager.HasAbility(Props.Ability) == true) ? 1 : 0);
+        }
+
         public override bool Interact(CharacterUnit source) {
             base.Interact(source);
 
