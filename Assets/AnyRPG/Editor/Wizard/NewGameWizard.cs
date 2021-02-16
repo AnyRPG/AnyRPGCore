fba1c137d2e103e7bd3259570e3d58587eb309fb
Andrew Steeley <andrew_steeley@hotmail.com>
2021-02-16 14:51:39 -0500

    Basic menu item and wizard with simple implementation that creates a game folder.

---
+++ Assets/AnyRPG/Editor/Wizard/NewGameWizard.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class NewGameWizard : ScriptableWizard
+{
+    // Will be a subfolder of Application.dataPath and should start with "/"
+    private const string newGameParentFolder = "/";
+
+    public string gameName = "";
+    public string gameVersion = "0.1a";
+    public string sceneName = "FirstScene";
+
+    [MenuItem("Tools/AnyRPG/New Game Wizard")]
+    static void CreateWizard()
+    {
+        ScriptableWizard.DisplayWizard<NewGameWizard>("New Game Wizard", "Create");
+    }
+
+    // Start is called before the first frame update
+    void OnWizardCreate()
+    {
+        // Create root game folder
+        string newGameFolder = GetNewGameFolder();
+        if (!System.IO.Directory.Exists(gameName))
+        {
+            System.IO.Directory.CreateDirectory(newGameFolder);
+        }
+        AssetDatabase.Refresh();
+    }
+
+    void OnWizardUpdate()
+    {
+        helpString = "Creates a new game based on the AnyRPG template";
+        errorString = Validate();
+        isValid = (errorString == null || errorString == "");
+    }
+
+    string GetNewGameFolder()
+    {
+        return Application.dataPath + newGameParentFolder + gameName;
+    }
+
+    string Validate()
+    {
+        if (gameName == null || gameName.Trim() == "")
+        {
+            return "Game name must not be empty";
+        }
+        string newGameFolder = GetNewGameFolder();
+        if (System.IO.Directory.Exists(newGameFolder))
+        {
+            return "Folder " + newGameFolder + " already exists.  Please delete this directory or choose a new game name";
+        }
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
