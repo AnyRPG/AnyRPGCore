6ceb7514309b0fd6152de37c8cbb8dfaad9beedb
Michael Day <github@mday.ca>
2022-03-09 02:04:53 -0800

    disable inline status effects

--- Assets/AnyRPG/Core/System/Scripts/Abilities/Effect/StatusEffectConfig.cs
+++ Assets/AnyRPG/Core/System/Scripts/Abilities/Effect/StatusEffectConfig.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 namespace AnyRPG {
 
     [System.Serializable]
-    public class StatusEffectConfig : AbilityEffectConfig {
+    public abstract class StatusEffectConfig : AbilityEffectConfig {
+        // abstract for now because we can't have inline status effects due to the need to do a database lookup to re-apply saved effects on game load
+        // making it abstract will prevent it from showing up on dropdown lists
 
         [SerializeField]
         private StatusEffectProperties effectProperties = new StatusEffectProperties();
