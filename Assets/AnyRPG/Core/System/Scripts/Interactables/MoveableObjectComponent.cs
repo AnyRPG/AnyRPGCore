7f7f2cbf057c7503e835bb098d49dfb47f4ed816
Michael Day <github@mday.ca>
2022-11-06 14:57:29 -0800

    fix floating point accuracy issue for vector3 math on moveable objects

--- Assets/AnyRPG/Core/System/Scripts/Interactables/MoveableObjectComponent.cs
+++ Assets/AnyRPG/Core/System/Scripts/Interactables/MoveableObjectComponent.cs
@@ -1,4 +1,5 @@
 using AnyRPG;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,7 +29,9 @@ namespace AnyRPG {
             interactableOptionProps.InteractionPanelTitle = "Interactable";
             if (Props.MoveableObject != null) {
                 originalPosition = Props.MoveableObject.transform.localPosition;
-                originalRotation = Props.MoveableObject.transform.localEulerAngles;
+                
+                // all angles are rounded to 4 decimals and than made positive to attempt to avoid rotations greater than 180 degrees
+                originalRotation = GetTranslatedEulerAngles(Props.MoveableObject.transform.localEulerAngles);
                 audioSource = Props.MoveableObject.GetComponent<AudioSource>();
             }
         }
@@ -55,15 +58,10 @@ namespace AnyRPG {
 
         public override bool Interact(CharacterUnit source, int optionIndex = 0) {
             //Debug.Log(gameObject.name + ".AnimatedObject.Interact(" + (source == null ? "null" : source.name) +")");
-            //if (coroutine != null) {
-                //Debug.Log(gameObject.name + ".AnimatedObject.Interact(): coroutine is not null, exiting");
-                //return false;
-            //}
+
             base.Interact(source, optionIndex);
             uIManager.interactionWindow.CloseWindow();
 
-            // loop through the animatedobjects prefabobjects
-            // check their state (open / closed)
             if (Props.MoveableObject == null) {
                 Debug.Log("MoveableObject.Interact(): gameObject was null. Check Inspector");
                 return false;
@@ -79,7 +77,6 @@ namespace AnyRPG {
             } else {
                 ChooseMovement();
             }
-            // lerp them to the other state, using the values defined in their sheathed and regular positions
 
             return false;
         }
@@ -123,41 +120,40 @@ namespace AnyRPG {
         }
 
         private IEnumerator AnimateObject(Vector3 newAngle, Vector3 newPosition, AudioClip audioClip) {
-            //Debug.Log("MoveableObjectComponent.AnimateObject(" + newAngle + ", " + newPosition + ")");
+            //Debug.Log($"MoveableObjectComponent.AnimateObject({newAngle.x} {newAngle.y} {newAngle.z}, {newPosition}) localEulerAngles: {Props.MoveableObject.transform.localEulerAngles.x}, {Props.MoveableObject.transform.localEulerAngles.y}, {Props.MoveableObject.transform.localEulerAngles.z}; position: {Props.MoveableObject.transform.localPosition}; objectopen = {objectOpen}");
 
-            newAngle = new Vector3(newAngle.x < 0 ? newAngle.x + 360 : newAngle.x, newAngle.y < 0 ? newAngle.y + 360 : newAngle.y, newAngle.z < 0 ? newAngle.z + 360 : newAngle.z);
-            //Quaternion originalRotation = interactable.SpawnReference.transform.localRotation;
-            //Vector3 originalPosition = interactable.SpawnReference.transform.localPosition;
-            //Debug.Log(gameObject.name + ".AnimatedObject.animateObject(" + newAngle + ", " + newPosition + "): original position: " + originalPosition + "; rotation: " + originalRotation);
+            newAngle = GetTranslatedEulerAngles(newAngle);
 
             if (audioSource != null && audioClip != null) {
                 //Debug.Log(gameObject.name + ".AnimatedObject.animateObject(): playing audioclip: " + audioProfile.AudioClip);
                 audioSource.PlayOneShot(audioClip);
             }
 
-            // testing doing this first to allow an object to reverse before its animation has completed
+            // setting open / closed state first to allow an object to reverse before its animation has completed
             objectOpen = !objectOpen;
 
             while (Props.MoveableObject.transform.localEulerAngles != newAngle || Props.MoveableObject.transform.localPosition != newPosition) {
-                //Debug.Log(gameObject.name + ".AnimatedObject.animateObject(" + newAngle + ", " + newPosition + "): localEulerAngles: " + interactable.MySpawnReference.transform.localEulerAngles + "; position: " + interactable.MySpawnReference.transform.localPosition);
-                //Quaternion newRotation = Quaternion.Lerp(originalRotation, Quaternion.Euler(newAngle), 0.01f);
-                //Quaternion newRotation = Quaternion.RotateTowards(interactable.MySpawnReference.transform.localRotation, Quaternion.Euler(newAngle), rotationSpeed);
-
-                // get a separate quaternion rotation to avoid issues with negative start angles
-                //Quaternion tmpRotation = interactable.MySpawnReference.transform.localRotation * Quaternion.Euler(newAngle);
-                //Vector3 realNewAngle = tmpRotation.eulerAngles;
+                //Debug.Log(Props.MoveableObject.name + ".AnimatedObject.animateObject(" + newAngle + ", " + newPosition + "): localEulerAngles: " + GetTranslatedEulerAngles(Props.MoveableObject.transform.localEulerAngles) + "; position: " + Props.MoveableObject.transform.localPosition);
 
                 Quaternion newRotation = Quaternion.RotateTowards(Quaternion.Euler(Props.MoveableObject.transform.localEulerAngles), Quaternion.Euler(newAngle), Props.RotationSpeed * Time.deltaTime);
-                //Quaternion newRotation = Quaternion.RotateTowards(interactable.MySpawnReference.transform.localRotation, Quaternion.Euler(realNewAngle), rotationSpeed);
-                //Quaternion newRotation = Quaternion.RotateTowards(interactable.MySpawnReference.transform.localRotation, tmpRotation, rotationSpeed);
                 Vector3 newLocation = Vector3.MoveTowards(Props.MoveableObject.transform.localPosition, newPosition, Props.MovementSpeed * Time.deltaTime);
+
                 Props.MoveableObject.transform.localPosition = newLocation;
                 Props.MoveableObject.transform.localRotation = newRotation;
+
                 yield return null;
             }
-            //objectOpen = !objectOpen;
-            //Debug.Log(gameObject.name + ".AnimatedObject.animateObject(" + newAngle + ", " + newPosition + "): localEulerAngles: " + interactable.MySpawnReference.transform.localEulerAngles + "; position: " + interactable.MySpawnReference.transform.localPosition + "; COMPLETE ANIMATION");
+
+            //Debug.Log(Props.MoveableObject.name + ".AnimatedObject.animateObject(" + newAngle + ", " + newPosition + "): localEulerAngles: " + Props.MoveableObject.transform.localEulerAngles + "; position: " + Props.MoveableObject.transform.localPosition + "; COMPLETE ANIMATION");
             moveCoroutine = null;
+        }
+
+        private Vector3 GetTranslatedEulerAngles(Vector3 originalAngle) {
+            // here all values are rounded to 4 decimals to avoid floating point accuracy issues that occur when adding whole numbers greater than 9
+            // eg : 3 + 0.123456 = 3.123456
+            // eg : 36 + 0.123456 = 36.12346
+            // eg : 360 + 0.123456 = 360.1234
+            return new Vector3(MathF.Round(originalAngle.x, 4) < 0f ? MathF.Round(originalAngle.x, 4) + 360f : MathF.Round(originalAngle.x, 4), MathF.Round(originalAngle.y, 4) < 0f ? MathF.Round(originalAngle.y, 4) + 360f : MathF.Round(originalAngle.y, 4), MathF.Round(originalAngle.z, 4) < 0f ? MathF.Round(originalAngle.z, 4) + 360f : MathF.Round(originalAngle.z, 4));
         }
 
         /*
