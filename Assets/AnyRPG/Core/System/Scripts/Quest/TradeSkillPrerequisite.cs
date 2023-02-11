031d02df30ca611f76f861d9887c5cdad885347a
Michael Day <github@mday.ca>
2023-02-11 11:43:22 -0800

    add resource selector to trade skill prerequisite

--- Assets/AnyRPG/Core/System/Scripts/Quest/TradeSkillPrerequisite.cs
+++ Assets/AnyRPG/Core/System/Scripts/Quest/TradeSkillPrerequisite.cs
@@ -10,6 +10,7 @@ namespace AnyRPG {
         public event System.Action OnStatusUpdated = delegate { };
 
         [SerializeField]
+        [ResourceSelector(resourceType = typeof(Skill))]
         private string prerequisiteName = string.Empty;
 
         private bool prerequisiteMet = false;
