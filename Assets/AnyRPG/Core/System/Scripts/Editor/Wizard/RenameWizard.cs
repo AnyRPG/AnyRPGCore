7de5c4f00b580bfac01cbb60be05776a6412a280
Michael Day <github@mday.ca>
2023-03-21 04:23:53 -0700

    add swappable mesh models to all EquipmentModelProfiles

--- Assets/AnyRPG/Core/System/Scripts/Editor/Wizard/RenameWizard.cs
+++ Assets/AnyRPG/Core/System/Scripts/Editor/Wizard/RenameWizard.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEditor;
-using UnityEditor.SceneManagement;
 
 namespace AnyRPG {
     public class RenameWizard : ScriptableWizard {
